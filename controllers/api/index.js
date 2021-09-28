5d8e62f83846a0456930c3dbb505ff0dba509602
bcbridges <bcb35b@gmail.com>
2021-09-28 12:20:05 -0600

    Added 4 initial routes for front end. Need to de-bug.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,63 @@
 const router = require("express").Router();
 // Bring in mongoose to interact with schmea
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const db = require("../../models");
+
+// getLastWorkout
+// GET all workouts - The api.js will get the last specific one
+// Can't test until the PUT and POST is Gucci :(
+router.get("/workouts", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $set: {
+        totalDuration: { $sum: "$exercices.duration" },
+      },
+    },
+  ]).then((res) => res.json(res));
+});
+
+// addExercise
+// PUT new exercise to a workout
+router.put("/workouts/:id", async (req, res) => {
+  await db.Workout.findOneAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    {
+      $push: {
+        exercises: req.body,
+      },
+    },
+    (err, data) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(data);
+      }
+    }
+  );
+});
+
+// createWorkout
+// POST new workout
+router.post("/workouts", async (req, res) => {
+  const response = await db.Workout.create({});
+  res.json(response);
+});
+
+// getWorkoutsInRange
+// GET all workouts within a range
+router.get("/workouts/range", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $set: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .limit(7)
+    .sort({ day: 1 })
+    .then((result) => res.json(result));
+});
 
 module.exports = router;
