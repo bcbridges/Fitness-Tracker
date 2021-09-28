36d4d7b1ed787dbd6edf9e1212c7cbcfd520481f
bcbridges <bcb35b@gmail.com>
2021-09-27 18:51:09 -0600

    Fixed error that was stopping the api dir from being accessed for routing.

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
+// Erroring out, fix ASAP
+const apiRoutes = require("./api");
+router.use("/api", apiRoutes);
 
+// If user navigates to home
 router.get("/", (req, res) => {
   res.sendFile("index.html", { root: "./public/" });
 });
 
+// When user clicks on the 'New Workout' btn
 router.get("/exercise", (req, res) => {
   res.sendFile("exercise.html", { root: "./public/" });
 });
 
+// If user clicks on the 'Dashboard' btn
 router.get("/stats", (req, res) => {
   res.sendFile("stats.html", { root: "./public/" });
 });
