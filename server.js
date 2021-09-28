471d99b4a2fefcf90764130d2765abee2cdc730b
bcbridges <bcb35b@gmail.com>
2021-09-28 13:13:01 -0600

    Updating server.js to connect to db.

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ app.use(require("./controllers"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/fitness_tracker_db",
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
 );
 
 app.listen(PORT, () => {
